95928843947156af46aab6a72769f3449013e1ea
abushahed670 <abu.shahed670@gmail.com>
2025-01-13 23:46:31 +0600

    looping technique

---
+++ looping.js
@@ -0,0 +1,13 @@
+/**
+ * looping technique
+ * 1. for loop
+ * 2. while loop 
+ * 3. do while
+ * 4. for in -- object loop korar jonno
+ * 5. for of -- array loop korar jonno 
+ * 
+ */
+const friends =['elon', 'bill', 'mark', 'waren'];
+for (const friend of friends){
+    console.log(friend);
+}
