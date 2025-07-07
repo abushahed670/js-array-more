0465f6cc079f150132150f657caa71f9562db6ca
abushahed670 <abu.shahed670@gmail.com>
2025-07-07 22:48:26 +0600

    reverse added

--- looping.js
+++ looping.js
@@ -9,5 +9,23 @@
  */
 const friends =['elon', 'bill', 'mark', 'waren'];
 for (const friend of friends){
-    console.log(friend);
+    //console.log(friend);
+}
+for(let i=0; i<friends.length; i++){
+    // console.log(i);
+    // console.log(friends[i]);
+}
+
+
+// we can do it by using while loop
+const numbers=[23, 34, 45, 43,78] 
+let i=0;
+while(i < friends.length){
+    // console.log(friends[i]);
+    i++;
+}
+let n=0;
+while(n<numbers.length){
+    console.log(numbers[n]);
+    n++;
 }
