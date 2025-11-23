508736468868c8bf254b2d56f5654348520a173e
abushahed670 <abu.shahed670@gmail.com>
2025-11-24 00:29:30 +0600

    sort

---
+++ sort.js
@@ -0,0 +1,18 @@
+const persons=['akib','rakib','sakib'];
+
+const sortedPerson=persons.sort();
+console.log(sortedPerson);
+
+// sort------>>>
+
+const numbers=[1,2,8,4,9,6];
+
+/*
+Ascending -----> smaller to larger
+Descending-----> larger to smaller 
+*/
+
+// const numbers_asc=numbers.sort()
+const numbers_dsc=numbers.sort()
+// console.log(numbers_asc);
+console.log(numbers_dsc);
